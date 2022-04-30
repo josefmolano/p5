c9984476f796281039044af8ddf7ee6fee738689
josefmolano <jf.molano1587@outlook.com>
2022-04-30 11:54:37 -0500

    bouncing

--- road-fighter/sketch1.js
+++ road-fighter/sketch1.js
@@ -2,7 +2,7 @@ function setup() {
     WIDHT = 530
     HEIGHT = 370
     SPACES = 10
-    speed = 5
+    speed_y = 5
     createCanvas(WIDHT, HEIGHT);
     value=0;
     y = 222;
@@ -26,14 +26,14 @@ function draw() {
     text(score, WIDHT-100, 50);
     for (var i=0;i<NUM_CARS;i++){
         text("🚙", car_coord[i][0], car_coord[i][1]);
-        car_coord[i][0]=car_coord[i][0]-speed
+        car_coord[i][0]=car_coord[i][0]-speed_y
         if(car_coord[i][0] > 0 & car_coord[i][0] < 20 & car_coord[i][1]==y){
             score = 0;
-            speed = 5;
+            speed_y = 5;
         }
         if(car_coord[i][0] < -40){
             score = score+1;
-            speed = speed + 0.1;
+            speed_y = speed_y + 0.1;
             if (i!=0){
                 car_coord[i][0] = car_coord[i-1][0]+WIDHT/4
                 car_coord[i][1] = (Math.floor(HEIGHT/SPACES)*Math.floor(Math.random()*SPACES))
