1f6f82070b91f4d9c8110c21e44be7489705ad4f
josefmolano <jf.molano1587@outlook.com>
2022-05-02 08:16:36 -0500

    bubble sort squares

--- sketch1.js
+++ sketch1.js
@@ -11,9 +11,9 @@ function setup() {
     WIDHT = 400;
     HEIGHT = 700;
     SIZE = 250;
-    frameRate(20);
+    //frameRate(30);
     createCanvas(WIDHT, HEIGHT);
-    N_NUMBERS = 25;
+    N_NUMBERS = 40;
     NUMBERS = [];
     polySynth = new p5.PolySynth();
     for (var n=0;n<N_NUMBERS;n++){
@@ -54,35 +54,36 @@ function setup() {
 
 function draw() {
     background(255);
-    noStroke();
+    //noStroke();
     if (frame<FRAMES.length){
         console.log(frame);
         for (var d=0;d<N_NUMBERS;d++){
             fill(int(FRAMES[frame][d]*255/N_NUMBERS));
-            arc(WIDHT/2, HEIGHT/2, SIZE, SIZE, d*2*PI/N_NUMBERS, (d+1)*2*PI/N_NUMBERS);
+            rect(WIDHT/2, HEIGHT-d*HEIGHT/N_NUMBERS, HEIGHT/N_NUMBERS, HEIGHT/N_NUMBERS);
             fill(0,0,0)
             text(
                 int(FRAMES[frame][d]*255/N_NUMBERS),
-                WIDHT/2+Math.cos((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2-10,
-                HEIGHT/2+Math.sin((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2
+                WIDHT/2+50,
+                HEIGHT-d*HEIGHT/N_NUMBERS-10
             )
             //polySynth.play('C2', 1, 0, 0.1);
         }
         if (PLAY_SOUND[frame]!=-1){
-            mySound[0].play(0,1,1,0.1,0.5);
+            //mySound[0].play(0,1,1,0.1,0.5);
         }
         frame++;
     }
     else {
         for (var d=0;d<N_NUMBERS;d++){
             fill(int(NUMBERS[d]*255/N_NUMBERS));
-            arc(WIDHT/2, HEIGHT/2, SIZE, SIZE, d*2*PI/N_NUMBERS, (d+1)*2*PI/N_NUMBERS);
+            rect(WIDHT/2, HEIGHT-d*HEIGHT/N_NUMBERS, HEIGHT/N_NUMBERS, HEIGHT/N_NUMBERS);
             fill(0,0,0)
             text(
                 int(NUMBERS[d]*255/N_NUMBERS),
-                WIDHT/2+Math.cos((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2-10,
-                HEIGHT/2+Math.sin((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2
+                WIDHT/2+50,
+                HEIGHT-d*HEIGHT/N_NUMBERS-10
             )
+            //polySynth.play('C2', 1, 0, 0.1);
         }
     }
 }
