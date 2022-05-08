a52ee1e96c956a10674c7f4da4c74d0482014f65
josefmolano <jf.molano1587@outlook.com>
2022-05-08 14:10:48 -0500

    quick sort bar

---
+++ quick-sort-pie/sketch1.js
@@ -0,0 +1,115 @@
+let polySynth;
+let mySound;
+let FRAMES = [];
+let PLAY_SOUND = [];
+
+function preload() {
+    mySound = [
+        loadSound("../sounds/slash-21834.mp3")
+    ];
+}
+
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function partition(arr, low, high) {
+ 
+    let pivot = arr[high];
+    let i = (low - 1);
+ 
+    for (let j = low; j <= high - 1; j++) {
+
+        var changed = false;
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr, i, j);
+            changed = true;
+        }
+    
+        snapshot = [];
+        for (var num=0;num<N_NUMBERS;num++){
+            snapshot.push(arr[num]);
+        }
+        FRAMES.push(snapshot);
+        if(changed){
+            PLAY_SOUND.push(1);
+        }
+        else {
+            PLAY_SOUND.push(-1);
+        }
+    }
+    swap(arr, i + 1, high);
+    
+    snapshot = [];
+    for (var num=0;num<N_NUMBERS;num++){
+        snapshot.push(arr[num]);
+    }
+    FRAMES.push(snapshot);
+    PLAY_SOUND.push(1);
+
+    return (i + 1);
+}
+ 
+function quickSort(arr, low, high) {
+    if (low < high) {
+        let pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+function setup() {
+    WIDHT = 400;
+    HEIGHT = 500;
+    SIZE = 250;
+    frameRate(30);
+    createCanvas(WIDHT, HEIGHT);
+    N_NUMBERS = 30;
+    NUMBERS = [];
+    polySynth = new p5.PolySynth();
+    for (var n=0;n<N_NUMBERS;n++){
+        NUMBERS.push(Math.random()*N_NUMBERS)
+    }
+    console.log(NUMBERS)
+
+    quickSort(NUMBERS,0,N_NUMBERS-1)
+    console.log(NUMBERS)
+
+    frame = 0;
+} 
+
+function draw() {
+    background(255);
+    if (frame<FRAMES.length){
+        console.log(frame);
+        for (var d=0;d<N_NUMBERS;d++){
+            fill(int(FRAMES[frame][d]*255/N_NUMBERS));
+            arc(WIDHT/2, HEIGHT/2, SIZE, SIZE, d*2*PI/N_NUMBERS, (d+1)*2*PI/N_NUMBERS);
+            fill(0,0,0)
+            text(
+                int(FRAMES[frame][d]*255/N_NUMBERS),
+                WIDHT/2+Math.cos((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2-10,
+                HEIGHT/2+Math.sin((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2
+            )
+        }
+        if (PLAY_SOUND[frame]!=-1){
+            mySound[0].play(0,1,1,0.1,0.5);
+        }
+        frame++;
+    }
+    else {
+        for (var d=0;d<N_NUMBERS;d++){
+            fill(int(NUMBERS[d]*255/N_NUMBERS));
+            arc(WIDHT/2, HEIGHT/2, SIZE, SIZE, d*2*PI/N_NUMBERS, (d+1)*2*PI/N_NUMBERS);
+            fill(0,0,0)
+            text(
+                int(NUMBERS[d]*255/N_NUMBERS),
+                WIDHT/2+Math.cos((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2-10,
+                HEIGHT/2+Math.sin((d+0.5)*2*PI/N_NUMBERS)*1.2*SIZE/2
+            )
+        }
+    }
+}
